9a3a17004c1c6eb369638ef7b1b3f005b244917f
Rashgaroth <50513263+rashgaroth@users.noreply.github.com>
2023-07-03 14:14:29 +0800

    Update src/config/endpoints.ts
    Co-authored-by: Ahmad Ali Abdilah <ahmad.ali.abdilah@gmail.com>

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -1,7 +1,7 @@
 import { HyperEndpoints } from "src/interfaces";
 
 const hyprEndpoints: HyperEndpoints = {
-  getItems: (id: string) => `/api/v1/tenant/items/${id}`,
+  item: `/api/v1/tenant/items/`,
   currency: `/api/v1/tenant/currencies`,
   collection: `/api/v1/tenant/collections`
 }
